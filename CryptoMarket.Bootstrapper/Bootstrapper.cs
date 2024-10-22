65f778cf1a7e034a830f809e617c3f6792411efa
KazzModan <kklopov64@gmail.com>
2024-10-22 19:40:33 +0300

    introduce bootstrapper

---
+++ CryptoMarket.Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using CryptoMarket.Views;
+using System.Windows;
+using System.Windows.Automation.Peers;
+
+namespace CryptoMarket.Bootstrapper
+{
+    public class Bootstrapper : IDisposable
+    {
+        public Window  Run ()
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            return mainWindow;
+        }
+        public void Dispose() { }
+    }
+}
--- CryptoMarket/App.xaml.cs
+++ CryptoMarket/App.xaml.cs
@@ -1,14 +1,25 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
-
 namespace CryptoMarket
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App 
     {
-    }
+        private Bootstrapper.Bootstrapper _bootstrapper;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+           _bootstrapper = new Bootstrapper.Bootstrapper();
+           MainWindow = _bootstrapper.Run();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _bootstrapper.Dispose();
+
+            base.OnExit(e);
+        }
+    }
 }
